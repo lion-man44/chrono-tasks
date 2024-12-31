cb098616a246be9a2c8c79178634ca3993c59887
lion-man44 <amanttou44@gmail.com>
2025-01-01 02:47:32 +0900

    Generalized id and timestamp

--- chrono_tasks/users/models.py
+++ chrono_tasks/users/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 from django.core.validators import EmailValidator, MaxLengthValidator, MinLengthValidator
+import random
 
-class User(models.Model):
+from shared.models import TimestampMixin, UUIDMixin
+
+class IconColorMixin(models.Model):
+    icon_color = models.CharField(max_length=6, validators=[MinLengthValidator(6), MaxLengthValidator(6)])
+
+    class Meta:
+        abstract = True
+
+class User(UUIDMixin, IconColorMixin, TimestampMixin):
+    """ユーザモデル
+
+    ユーザのデータを作成する際に使用するモデル
+
+    Args:
+        UUIDMixin (models.Model): UUIDを主キーに設定するミックスイン
+        IconColorMixin (models.Model): アイコンのカラーを設定するミックイン
+        TimestampMixin (models.Model): 作成日時と更新日時を設定するミックイン
+    """
+
     name = models.CharField(max_length=100, validators=[MaxLengthValidator(100)])
     email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator(), MaxLengthValidator(255)])
-    icon_color = models.CharField(max_length=7, validators=[MinLengthValidator(7), MaxLengthValidator(7)])
     password = models.CharField(max_length=255)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
 
     class Meta:
         db_table = 'users'
