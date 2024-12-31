b0eb1c8d5ab7129f406ae0b6d90c418b815d48d1
lion-man44 <amanttou44@gmail.com>
2024-12-31 22:44:45 +0900

    Created migration file for user

--- chrono_tasks/users/admin.py
+++ chrono_tasks/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'email', 'icon_color', 'created_at', 'updated_at')
+  search_fields = ('id', 'name', 'email', 'created_at')
+  ordering = ('-created_at',)
--- chrono_tasks/users/models.py
+++ chrono_tasks/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import EmailValidator, MaxLengthValidator, MinLengthValidator
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100, validators=[MaxLengthValidator(100)])
+    email = models.EmailField(max_length=255, unique=True, validators=[EmailValidator(), MaxLengthValidator(255)])
+    icon_color = models.CharField(max_length=7, validators=[MinLengthValidator(7), MaxLengthValidator(7)])
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'users'
