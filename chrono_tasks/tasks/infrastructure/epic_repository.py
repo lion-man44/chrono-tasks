e3221f214f662a203bc5e73f2355cddfa4500275
lion-man44 <amanttou44@gmail.com>
2025-01-05 23:11:39 +0900

    Removed unnecessary line

--- chrono_tasks/tasks/infrastructure/epic_repository.py
+++ chrono_tasks/tasks/infrastructure/epic_repository.py
@@ -1,5 +1,3 @@
-from uuid import uuid4
-
 from tasks.domain.epic_i_repository import EpicIRepository
 
 from tasks.usecase.epic_dto import EpicInputDTO
@@ -7,9 +5,6 @@ from tasks.domain.epic_entity import EpicEntity
 from tasks.models import Epic
 
 class EpicRepository(EpicIRepository):
-  def __init__(self, epic_repository: EpicIRepository) -> None:
-    self.epic_repository = epic_repository
-
   def save(self, epic_input_dto: EpicInputDTO) -> EpicEntity:
     epic = EpicEntity(epic_input_dto)
     Epic.objects.update_or_create(defaults=epic)
